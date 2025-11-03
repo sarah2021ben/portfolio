c7db21189923776886b1f958a0403171e643da49
sarah_ben <benkanoun.sarah@gmail.com>
2025-11-03 17:26:13 +0100

    adding contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,48 @@
-import React from 'react';
-
+import React from "react";
+import { BsArrowRight } from "react-icons/bs";
 
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div className="h-full bg-primary/30">
+      <div className="container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full">
+        <div className="flex flex-col w-full max-w-[700px]">
+          <h2 className="h2 text-center mb-12">
+            Let&apos;s <span className="text-accent">connect.</span>
+          </h2>
+          {/**form */}
+          <form className="flex-1 flex flex-col gap-6 w-full mx-auto">
+            <div className="flex gap-x-6 w-full">
+              <input
+                id="name"
+                type="text"
+                placeholder="name"
+                className="input"
+              />
+              <input
+                id="email"
+                type="email"
+                placeholder="email"
+                className="input"
+              />
+            </div>
+            <input type="text" placeholder="subject" className="input" />
+            <textarea placeholder="message" className="textarea"></textarea>
+            <button
+              className="btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300
+           flex items-center justify-center overflow-hidden hover:border-accent group cursor-pointer"
+            >
+              <span className="group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500">
+                {" "}
+                Let&apos;s talk
+              </span>
+
+              <BsArrowRight className="translate-y-[0%] opacity-0 group-hover:flex group-hover:-translate-y- group-hover:opacity-100 transition-all duration-300 absolute text-[22px]" />
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
